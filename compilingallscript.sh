25abddc4920379501cbbc3de0a2d1a98226e04ff
johnivarhauge <35365734+johnivarhauge@users.noreply.github.com>
2018-01-25 11:36:57 +0100

    Add files via upload

---
+++ compilingallscript.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cd /home/s882765/Oppgaver/overblikk/
+FILES=*
+for f in $FILES
+do
+   if [[ "$f" == *.py ]]; then    
+     echo "Processing $f file..."
+     python $f 
+   elif [[ "$f" == *.java ]]; then
+     echo "Processing $f file..."
+     javac $f
+     java ${f::-5}
+   elif [[ "$f" == *.c ]]; then
+     echo "Processing $f file..."
+     gcc $f
+     ./a.out
+   elif [[ "$f" == *.o ]]; then
+     echo "Processing $f file..."
+     gcc $f
+    ./a.out
+   elif [[ "$f" == *.sh ]]; then
+     echo "Processing $f file..."
+     ./$f
+   elif [[ "$f" == *.s ]]; then
+     echo "Processing $f file..."
+     gcc $f
+    ./a.out
+   fi
+done
